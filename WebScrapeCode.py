631d74e1876e5d0523966bcadbff597fd6ce6875
ojames22 <66024854+ojames22@users.noreply.github.com>
2020-05-28 15:36:04 -0400

    Create WebScrapeCode.py
    Started making an interative beginning (which does not quite work) and continued the lessons on codeacademy

---
+++ WebScrapeCode.py
@@ -0,0 +1,9 @@
+def welcome():
+	username = input("Enter username:")
+
+	print("Hello " + username + "! Please enter a date for games and starts.")
+
+	date_value = input("Enter date:")
+	print("Date: " + date_value)
+
+welcome()
