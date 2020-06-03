74f554ea6b6058e7172e352ca37737192a248daf
ojames22 <66024854+ojames22@users.noreply.github.com>
2020-06-03 00:24:34 -0400

    Update WebScrapeCode.py
    Code is extremely messy and needs immense restructuring, but I think this is a good start. The code under function "print_games" and specifically the location of section/index needs to be modified

--- WebScrapeCode.py
+++ WebScrapeCode.py
@@ -1,4 +1,5 @@
 import requests
+from bs4 import BeautifulSoup
 
 def username():
 	username = input("Enter username: ")
@@ -12,6 +13,52 @@ def getPersonName():
 	name = "G"
 	return name
 
-URL = 'https://www.monster.com/jobs/search/?q=Software-Developer&where=Australia'
-page = requests.get(URL)
-print(page)
+def scrape_jobs(location=None):
+    """Notes from RealPython.com:
+    :param location: Where the job is located
+    :type location: str
+    :return: all job postings from first page that match the search results
+    :rtype: BeautifulSoup object
+    """
+    if location:
+        URL = (
+             'https://www.basketball-reference.com/boxscores/'
+             '?month&day&year{location}'
+        )
+    else:
+        URL = 'https://www.basketball-reference.com/boxscores/?month&day&year'
+    page = requests.get(URL)
+
+    soup = BeautifulSoup(page.content, "html.parser")
+    results = soup.find(id="ResultsContainer")
+    return results
+
+def find_elements():
+	results = soup.find(id='month')
+
+	results = soup.find(id='day')
+
+	results = soup.find(id='year')
+
+	return results
+
+def print_games(results):
+	game_elems = results.find_all("section", class_="index")
+
+	for game_elem in game_elems:
+    month_elem = game_elem.find('h2', class_='month')
+    day_elem = game_elem.find('div', class_='day')
+    year_elem = game_elem.find('div', class_='year')
+    print(month_elem.text)
+    print(day_elem.text)
+    print(year_elem.text)
+    print()
+
+#print(page)
+''' Evidently, I am missing an interface in which the month, day, and year can be entered into.
+Unless I further behind in the process
+'''
+
+#https://www.basketball-reference.com/boxscores/?month=1&day=1&year=2000
+
+#class
