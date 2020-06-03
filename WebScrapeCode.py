954991eb7adbf85890b21ef48174de1f0ec2d8dd
ojames22 <66024854+ojames22@users.noreply.github.com>
2020-06-02 23:15:36 -0400

    June 2
    I've basically been reading through the tutorial and getting out of it as much as possible.

--- WebScrapeCode.py
+++ WebScrapeCode.py
@@ -1,9 +1,17 @@
-def welcome():
-	username = input("Enter username:")
+import requests
+
+def username():
+	username = input("Enter username: ")
 
 	print("Hello " + username + "! Please enter a date for games and starts.")
 
 	date_value = input("Enter date:")
 	print("Date: " + date_value)
 
-welcome()
+def getPersonName():
+	name = "G"
+	return name
+
+URL = 'https://www.monster.com/jobs/search/?q=Software-Developer&where=Australia'
+page = requests.get(URL)
+print(page)
