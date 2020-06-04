0aa8303ee47390c4c06fdd3c9d49d9c62d186964
ojames22 <66024854+ojames22@users.noreply.github.com>
2020-06-04 13:15:52 -0400

    Everything is working except for user error. Made user interaction smoother, print layout more clear and got rid of some unnecessary aspects

--- WebScrapeCode.py
+++ WebScrapeCode.py
@@ -4,24 +4,24 @@ from bs4 import BeautifulSoup
 def username():
 	username = input('Enter username: ')
 
-	print('Hello ' + username + '! Please enter a date for games and starts.')
+	print('Hello ' + username + '! Please enter values for month, day, and year for games and stats.')
 
-	date_value = input('Enter date:')
-	print('Date: ' + date_value)
+	#date_value = input('Enter date:')
+	#print('Date: ' + date_value)
 
-def get_date():
-	month = input('Enter month: ')
-	day = input('Enter day: ')
-	year = input('Enter year: ')
+#def get_date():
+#	month = input('Enter month: ')
+#	day = input('Enter day: ')
+#	year = input('Enter year: ')
 
-	URL = 'https://www.basketball-reference.com/boxscores/?month=' + month + '&day=' + day +'&year=' + year
-	page = requests.get(URL)
-	print(URL)
+#	URL = 'https://www.basketball-reference.com/boxscores/?month=' + month + '&day=' + day +'&year=' + year
+#	page = requests.get(URL)
+#	print(URL)
 
-def scrape_games(location=None):
-	month = input('Enter month: ')
-	day = input('Enter day: ')
-	year = input('Enter year: ')
+def scrape_games():
+	month = input('Enter month (--): ')
+	day = input('Enter day (--): ')
+	year = input('Enter year (----): ')
 
 	"""Notes from RealPython.com:
 	:param location: Where the job is located
@@ -29,17 +29,12 @@ def scrape_games(location=None):
 	:return: all job postings from first page that match the search results
 	:rtype: BeautifulSoup object
 	"""
-	if location:
-		URL = (
-			'https://www.basketball-reference.com/boxscores/'
-			'?month&day&year{location}'
-		)
-	else:
-		URL = 'https://www.basketball-reference.com/boxscores/?month=' + month + '&day=' + day +'&year=' + year
+	URL = 'https://www.basketball-reference.com/boxscores/?month=' + month + '&day=' + day +'&year=' + year
+	print(URL)
 	page = requests.get(URL)
 
 	soup = BeautifulSoup(page.content, 'html.parser')
-	results = soup.find('div', {'class": "game_summaries'})
+	results = soup.find('div', class_='game_summaries')
 	return results
 
 #def find_elements(data):
@@ -49,28 +44,30 @@ def scrape_games(location=None):
 	#return results
 
 def print_games(results):
-	game_elems = results.find('div', {'class": "game_summary'})
+	game_elems = results.find_all('div', class_='game_summary')
 
 	for game_elem in game_elems:
-		#print(game_elem, end='\n'*2)
-		winner_elem = game_elem.find('div', class_='winner')
-		winnerScore_elem = game_elem.find('td', class_='right')
-		loser_elem = game_elem.find('div', class_='loser')
-		loserScore_elem = game_elem.find('td',class_='right')
-		#if None in (winner_elem,winnerScore_elem,loser_elem,loserScore_elem):
-	else:
-		print('No games were played on this date.')
-		print(winner_elem.text.strip)#Previously month_elem.text
-		print(winnerScore_elem.text.strip)#Previously day_elem.text
-		print(loser_elem.text.strip)#Previously year_elem.text
-		print(loserScore_elem.text.strip)#Previously game_elem.text
-		print()
+
+		winner_elem = game_elem.find('tr', class_='winner')
+		winner_name = winner_elem.find('a').get_text()
+		winner_score = winner_elem.find('td', class_='right').get_text()
+
+		loser_elem = game_elem.find('tr', class_='loser')
+		loser_name = loser_elem.find('a').get_text()
+		loser_score = loser_elem.find('td', class_='right').get_text()
+
+		print('\nW: '+winner_name)
+		print(winner_score)
+		print('L: '+loser_name)
+		print(loser_score)
+		if None in (winner_elem,winner_score,loser_elem,loser_score):
+			print('No games were played on this date.')
+
 
 #Call scrape games
+username()
 results = scrape_games()
 print_games(results)
 #print(print_games)
 #Pass results to print_games
 #print(results)
-
-#https://www.basketball-reference.com/boxscores/?month=1&day=1&year=2000
